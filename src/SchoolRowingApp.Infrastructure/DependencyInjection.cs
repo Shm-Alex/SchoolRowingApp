fdc5daed2411d4746f1000732e875aa33b199f16
shmykov.alexander <shmykov.alexander@gmail.com>
2025-08-31 15:52:36 +0300

    Di Востановил прежнее не смотрел что там

--- src/SchoolRowingApp.Infrastructure/DependencyInjection.cs
+++ src/SchoolRowingApp.Infrastructure/DependencyInjection.cs
@@ -10,78 +10,78 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Extensions.DependencyInjection;
-public static class DependencyInjection
-{
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-    {
-        var connectionString = configuration.GetConnectionString("DefaultConnection")
-            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(connectionString));
-
-        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
-
-        return services;
-    }
-}
 //public static class DependencyInjection
 //{
-//    public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
+//    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
 //    {
-//        var connectionString = builder.Configuration.GetConnectionString("CleanArchitectureDb");
-//        Guard.Against.Null(connectionString, message: "Connection string 'CleanArchitectureDb' not found.");
+//        var connectionString = configuration.GetConnectionString("DefaultConnection")
+//            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-//        builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
-//        builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+//        services.AddDbContext<ApplicationDbContext>(options =>
+//            options.UseNpgsql(connectionString));
 
-//        builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
-//        {
-//            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
-//#if (UsePostgreSQL)
-//            options.UseNpgsql(connectionString);
-//#elif (UseSqlite)
-//            options.UseSqlite(connectionString);
-//#else
-//            options.UseSqlServer(connectionString);
-//#endif
-//            options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
-//        });
+//        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
-//#if (UseAspire)
-//#if (UsePostgreSQL)
-//        builder.EnrichNpgsqlDbContext<ApplicationDbContext>();
-//#elif (UseSqlServer)
-//        builder.EnrichSqlServerDbContext<ApplicationDbContext>();
-//#endif
-//#endif
-
-//        builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
-
-//        builder.Services.AddScoped<ApplicationDbContextInitialiser>();
-
-//#if (UseApiOnly)
-//        builder.Services.AddAuthentication()
-//            .AddBearerToken(IdentityConstants.BearerScheme);
-
-//        builder.Services.AddAuthorizationBuilder();
-
-//        builder.Services
-//            .AddIdentityCore<ApplicationUser>()
-//            .AddRoles<IdentityRole>()
-//            .AddEntityFrameworkStores<ApplicationDbContext>()
-//            .AddApiEndpoints();
-//#else
-//        builder.Services
-//            .AddDefaultIdentity<ApplicationUser>()
-//            .AddRoles<IdentityRole>()
-//            .AddEntityFrameworkStores<ApplicationDbContext>();
-//#endif
-
-//        builder.Services.AddSingleton(TimeProvider.System);
-//        builder.Services.AddTransient<IIdentityService, IdentityService>();
-
-//        builder.Services.AddAuthorization(options =>
-//            options.AddPolicy(Policies.CanPurge, policy => policy.RequireRole(Roles.Administrator)));
+//        return services;
 //    }
 //}
+public static class DependencyInjection
+{
+    public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("CleanArchitectureDb");
+        Guard.Against.Null(connectionString, message: "Connection string 'CleanArchitectureDb' not found.");
+
+        builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+#if (UsePostgreSQL)
+            options.UseNpgsql(connectionString);
+#elif (UseSqlite)
+            options.UseSqlite(connectionString);
+#else
+            options.UseSqlServer(connectionString);
+#endif
+            options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        });
+
+#if (UseAspire)
+#if (UsePostgreSQL)
+        builder.EnrichNpgsqlDbContext<ApplicationDbContext>();
+#elif (UseSqlServer)
+        builder.EnrichSqlServerDbContext<ApplicationDbContext>();
+#endif
+#endif
+
+        builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+
+#if (UseApiOnly)
+        builder.Services.AddAuthentication()
+            .AddBearerToken(IdentityConstants.BearerScheme);
+
+        builder.Services.AddAuthorizationBuilder();
+
+        builder.Services
+            .AddIdentityCore<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddApiEndpoints();
+#else
+        builder.Services
+            .AddDefaultIdentity<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+#endif
+
+        builder.Services.AddSingleton(TimeProvider.System);
+        builder.Services.AddTransient<IIdentityService, IdentityService>();
+
+        builder.Services.AddAuthorization(options =>
+            options.AddPolicy(Policies.CanPurge, policy => policy.RequireRole(Roles.Administrator)));
+    }
+}
